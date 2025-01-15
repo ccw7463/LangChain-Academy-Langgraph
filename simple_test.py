70fee367bfc5b62168c7803e7d9222db9e4e7aa3
ccw7463 <ccw7463@example.com>
2025-01-15 14:53:03 +0900

    update simple test

--- simple_test.py
+++ simple_test.py
@@ -1,22 +1,12 @@
-import os
 import uuid
 import time
-from dotenv import load_dotenv
-from functools import wraps
 import argparse
-from ml_collections import ConfigDict
 from langgraph.graph import StateGraph, START, END, MessagesState
-from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.store.memory import InMemoryStore
-from langgraph.store.base import BaseStore
-from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_huggingface import ChatHuggingFace
-from langchain_core.runnables import RunnableConfig
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage, RemoveMessage
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_core.messages import SystemMessage
 
 RESET = "\033[0m"        # Reset to default
 RED = "\033[91m"         # Bright Red
@@ -24,78 +14,6 @@ BLUE = "\033[94m"        # Bright Blue
 GREEN = "\033[92m"        # Bright Green
 YELLOW = "\033[93m"       # Bright Yellow
 PINK = "\033[95m"         # Bright Pink
-
-def trace_function(enable_print=True, only_func_name=False):
-    def wrapper(func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            if enable_print:
-                if only_func_name:
-                    print(f"\n... Passing Through [{func.__name__}] ...")
-                else:
-                    print(f"\n... Passing Through [{func.__name__}] ...")
-                    print(f"{RED}#### [Input State]{RESET}")
-                    print(f"  args: {args}")
-                    print(f"  kwargs: {kwargs}")
-            result = func(*args, **kwargs)  # 원본 함수 호출
-            if enable_print:
-                if only_func_name:
-                    pass
-                else:
-                    print(f"\n{BLUE}#### [Output State]{RESET}")
-                    print(f"  result: {result}")
-            return result
-        return wrapped
-    return wrapper
-
-class ToolConversation:
-    def __init__(self, llm):
-        self.system_prompt = "You are a helpful assistant tasked with performing arithmetic on a set of inputs."
-        self.tools = [self._tool_add]
-        self.llm = llm
-        self.llm_with_tools = self.llm.bind_tools(self.tools)
-        self._build_graph()
-
-    def __call__(self,
-                 messages):
-        return self._call_graph(messages)
-
-    def _call_graph(self, 
-                    messages):
-        """
-            Des:
-                그래프 호출 함수
-        """
-        return self.graph.invoke({"messages": messages})
-
-    def _build_graph(self):
-        """
-            Des:
-                그래프 생성함수
-        """
-        builder = StateGraph(MessagesState)
-        builder.add_node("_node_assistant", self._node_assistant)
-        builder.add_node("tools", ToolNode(self.tools))
-        builder.add_edge(START, "_node_assistant")
-        builder.add_conditional_edges("_node_assistant", tools_condition)
-        builder.add_edge("tools", "_node_assistant")
-        self.graph = builder.compile()
-
-    @trace_function(enable_print=True, only_func_name=True)
-    def _node_assistant(self, state:MessagesState):
-        return {"messages": [self.llm_with_tools.invoke([SystemMessage(content=self.system_prompt)] + state["messages"])]}
-
-    @trace_function(enable_print=True, only_func_name=True)
-    def _tool_add(self,
-                  a: int, 
-                  b: int) -> int:
-        """Adds a and b.
-
-        Args:
-            a: first int
-            b: second int
-        """
-        return a + b
     
 class ConversationTest:
     def __init__(self, llm):
@@ -130,7 +48,6 @@ class ConversationTest:
         builder.add_edge("_node_answer", END)
         self.graph = builder.compile(checkpointer=self.ShortTermMemory)
 
-    @trace_function(enable_print=True, only_func_name=True)
     def _node_answer(self, 
                     state: MessagesState):
         """
@@ -154,44 +71,23 @@ class ConversationTest:
 
 
 def main(args):
-    if args.model_type == "huggingface":
-        llm_endpoint = HuggingFaceEndpoint(
-            endpoint_url=args.endpoint_url, # Endpoint 주소 (TGI 사용)
-            huggingfacehub_api_token=os.getenv("HF_API_TOKEN"), # HuggingFace API 토큰
-            max_new_tokens=4096,
-            top_k=1,
-            top_p=0.001,
-            temperature=0.001,
-            repetition_penalty=1.03,
-            stop_sequences=['<end_of_turn>','<eos>']
-        )
-        llm = ChatHuggingFace(
-            llm=llm_endpoint,
-            model_id="rtzr/ko-gemma-2-9b-it",
-            stream_mode=True
-        ).bind(max_tokens=4096)
-    else:
-        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-        llm = ChatOpenAI(model="gpt-4o")
+    llm_endpoint = HuggingFaceEndpoint(
+        endpoint_url=args.endpoint_url,
+        max_new_tokens=2048,
+        top_k=1,
+        top_p=0.001,
+        temperature=0.001,
+        repetition_penalty=1.03,
+        stop_sequences=['<end_of_turn>','<eos>']
+    )
+    llm = ChatHuggingFace(
+        llm=llm_endpoint,
+        model_id="rtzr/ko-gemma-2-9b-it",
+        stream_mode=True
+    ).bind(max_tokens=2048)
     
-    tool_test(llm)
-    chat_test(llm)
-
-def tool_test(llm):
-    tool_conversation = ToolConversation(llm)
-    print(f"{GREEN}=========================================={RESET}")
-    print(f"{GREEN}  [Langgraph 기반 툴 테스트를 시작합니다]{RESET}")
-    print(f"{GREEN}=========================================={RESET}")
-    messages = "Add 3 and 4"
-    print(f" {YELLOW}[요청 메시지] : {messages}{RESET}")
-    messages = tool_conversation(messages)
-    for message in messages['messages']:
-        if type(message).__name__ == "ToolMessage":
-            print(f"\n{BLUE}### [중간 계산 결과] :\n {message.content}{RESET}\n")
-    print(f"{BLUE}### [답변 메시지] :\n {messages['messages'][-1].content.strip()}{RESET}\n")
-    
-def chat_test(llm):
     conversation = ConversationTest(llm)
+    
     print(f"{GREEN}=========================================={RESET}")
     print(f"{GREEN} [Langgraph 기반 채팅 테스트를 시작합니다]{RESET}")
     print(f"{GREEN}=========================================={RESET}")
@@ -209,7 +105,6 @@ if __name__ == "__main__":
     start_time = time.time()
     parser = argparse.ArgumentParser()
     parser.add_argument("--endpoint_url", type=str, default="http://192.168.1.21:11980")
-    parser.add_argument("--model_type", type=str, default="gpt")
     args = parser.parse_args()
     main(args)
     end_time = time.time()
